7df643e86b6e4c2ae9b3cddea38c94ec080b4583
Ana <anavelezrueda@gmail.com>
2020-04-08 16:54:44 -0300

    cursada 2020 tp vida tres dimensiones

--- Modelado/Ejemplos_corrida/FASTAtoPIR.py
+++
@@ -1,8 +0,0 @@
-from modeller import *
-e = environ()
-
-target='pdb_70'
-
-a = alignment(e, file='%s.fasta'%target, alignment_format='FASTA')
-a.write(file='%s.ali'%target, alignment_format='PIR')
-
